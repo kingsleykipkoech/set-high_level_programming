1686561e2eba69b7ded89e3d73d6825eefb7bf0b
Kingsley Kipkoech <kingsleykoech@gmail.com>
2026-08-07 15:02:44 +0200

    Fix 101-stats.py and add complete test cases to 101-lazy_matrix_mul.txt

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Multiplies 2 matrices using numpy.
+    """Multiplies 2 matrices using NumPy with input validation.
 
     Args:
         m_a: first matrix
@@ -13,4 +13,39 @@ def lazy_matrix_mul(m_a, m_b):
     Returns:
         numpy matrix result of m_a * m_b
     """
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    if not all(isinstance(r, list) for r in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(r, list) for r in m_b):
+        raise TypeError("m_b must be a list of lists")
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+    for row in m_a:
+        for el in row:
+            if not isinstance(el, (int, float)):
+                raise TypeError(
+                    "m_a should contain only integers or floats"
+                )
+    for row in m_b:
+        for el in row:
+            if not isinstance(el, (int, float)):
+                raise TypeError(
+                    "m_b should contain only integers or floats"
+                )
+    row_len_a = len(m_a[0])
+    for row in m_a:
+        if len(row) != row_len_a:
+            raise TypeError("each row of m_a must be of the same size")
+    row_len_b = len(m_b[0])
+    for row in m_b:
+        if len(row) != row_len_b:
+            raise TypeError("each row of m_b must be of the same size")
+    if row_len_a != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
     return np.matmul(m_a, m_b)
