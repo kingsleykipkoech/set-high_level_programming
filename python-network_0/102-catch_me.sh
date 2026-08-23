c44edda661e3d7e12b37fd4b504e5c87832be393
Kingsley Kipkoech <kingsleykoech@gmail.com>
2026-08-23 22:05:31 +0200

    Fix 102-catch_me.sh: send user_id=98 cookie

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request that causes the server to respond with "You got me!"
-curl -s -L -d "" -X PUT -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -s -b "user_id=98" 0.0.0.0:5000/catch_me
