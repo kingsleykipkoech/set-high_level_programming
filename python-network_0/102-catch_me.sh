27f77967fb46043e6fd71cc73a556bb8f44ff98b
Kingsley Kipkoech <kingsleykoech@gmail.com>
2026-08-23 22:14:07 +0200

    Fix 102-catch_me.sh: PUT method + user_id cookie

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request that causes the server to respond with "You got me!"
-curl -s -b "user_id=98" -X GET 0.0.0.0:5000/catch_me
+curl -s -b "user_id=98" -X PUT 0.0.0.0:5000/catch_me
